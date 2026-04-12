da48ea41fd5fe09b42661bd1a3f5a40cd33fddc1
kashif <kashusays1@gmail.com>
2026-04-12 16:54:10 +0400

    test-1

--- src/main/java/com/car/carservices/controller/BrListController.java
+++ src/main/java/com/car/carservices/controller/BrListController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class BrListController {
-
+    // test
     private final BrListService service;
 
     public BrListController(BrListService service) {
